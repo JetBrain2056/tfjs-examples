d8a03ad4ec7747675fc60ce6ca7d6401e79f8220
{JetBrain} <108869575+JetBrain2056@users.noreply.github.com>
2024-03-30 19:12:27 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express            = require('express');
+
+const port   = process.env.PORT||9000;
+const host   = process.env.HOST||'0.0.0.0'||'127.0.0.1';
+const server = express();
+
+server.use(express.static('./app'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.use("/@tensorflow"       , express.static('./node_modules/@tensorflow')); 
+server.use("/@tensorflow-models", express.static('./node_modules/@tensorflow-models')); 
+
+//assuming app is express Object.
+server.get('/',function(req,res) {
+    res.render('index.html');
+  });  
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
